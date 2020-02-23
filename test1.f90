3e63abb1261202918ba8796f8ecba3860523b775
Shubham Singh <shubham.singh091@gmail.com>
2020-02-23 14:43:39 -0600

    added addition to test code

--- test1.f90
+++ test1.f90
@@ -2,6 +2,19 @@ program main
 
 implicit none
 
-  print*, "Hello World!"
+! Declare Variables here
+real*8 :: var1, var2, var3
+
+
+! Execute Code here
+
+var1 = 0.51
+var2 = 0.78
+
+var3 = var1+var2
+
+print*, "Hello World!"
+print*, "variable 3 value is", var3
+!pause
 
 end program main
